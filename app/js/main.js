4fb0b5001e5d26dd067d3b5c8f0238dcefd4fee4
Alimzhanov Amir <alimzhanovamir@mail.ru>
2018-01-31 02:12:20 +0300

    test 3, snap create pathCircle

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,19 @@
 var s = Snap("#svg");
-var cx = 130;
-var cy = 130;
-var r  = 130;
+
 function circlePath(cx, cy, r){
+	var cx = 134;
+	var cy = 134;
+	var r  = 110;
     return 'M '+cx+' '+cy+' m -'+r+', 0 a '+r+','+r+' 0 1,0 '+(r*2)+',0 a '+r+','+r+' 0 1,0 -'+(r*2)+',0';
 }
+var total = circlePath();
+s.path(total).attr({
+	fill: 'none',
+	stroke:'#0ff',
+	strokeWidth:5
+})
+
+
 
 // function animateLoop(){
 // 	$("#outline").css("stroke-dasharray", $(".path")[0].getTotalLength());
