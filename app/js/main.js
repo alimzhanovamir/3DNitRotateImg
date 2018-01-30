ad0682a80d3c56f1249934808928d25be888f360
Alimzhanov Amir <alimzhanovamir@mail.ru>
2018-01-30 20:37:16 +0300

    test-1

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+var s = Snap("#svg");
+var cp = s.circle(130,130,127).attr({ 
+	fill: "none", 
+	stroke: "#000",
+	strokeWidth: 8 
+});
+
+var cp2 = s.circle(130,130,127).attr({ 
+	fill: "none", 
+	stroke: "yellow",
+	strokeWidth: 4 
+});
